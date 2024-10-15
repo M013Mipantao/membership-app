0fcf410d458b764990609d18a70eb45042c70039
Mohammad Michael U. Mipantao <mumipantao@alphaland.com.ph>
2024-10-15 10:59:49 +0800

    change the name to balesin key wallet

--- resources/views/layouts/scanning_page.blade.php
+++ resources/views/layouts/scanning_page.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Membership (still no idea with the name)</title>
+    <title>Balesin Key Wallet</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">   
     <!-- Custom fonts for this template-->
--- resources/views/layouts/single_page_ui.blade.php
+++ resources/views/layouts/single_page_ui.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Membership (still no idea with the name)</title>
+    <title>Balesin Key Wallet</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">   
     <!-- Custom fonts for this template-->
     <link href="{{ asset('sb-admin-2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
